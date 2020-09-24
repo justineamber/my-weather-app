a3ccf6ea9600ca0964c34b89f3f3815ae19b55b1
Justine Hill <justinehill@Justines-MacBook-Pro.local>
2020-09-24 10:27:08 +0200

    component's updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Image from "../src/static/cloudyWeather.jpg";
 import WeatherDasboard from "./components/Weather-Dashboard/Weather-Dashboard";
 import "./App.css";
 
@@ -10,14 +8,6 @@ const useStyles = makeStyles({
     flexGrow: 1,
     height: "100vh"
   },
-  paper: {
-    width: "100%",
-    height: "100%",
-    backgroundImage: `url(${Image})`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center center"
-  }
 });
 
 function App() {
@@ -26,9 +16,7 @@ function App() {
   return (
     <div className="App">
       <div className={classes.root}>
-        <Paper className={classes.paper} square={true}>
           <WeatherDasboard  />
-        </Paper>
       </div>
     </div>
   );
--- src/components/Weather-Dashboard/Weather-Dashboard.js
+++ src/components/Weather-Dashboard/Weather-Dashboard.js
@@ -6,8 +6,18 @@ import SearchComponent from "../Search-Component/Search-Component";
 import CurrentCityConditionsDisplayComponent from "../Current-City-Conditions-Display-Component/Current-City-Conditions-Display-Component";
 import ForecastTabsComponent from "../Forecast-Tabs-Component/Forecast-Tabs-Component";
 import { Box } from "@material-ui/core";
+import Image from "../../../src/static/overcastCloudy.jpg";
+import Unsplash, { toJson }  from 'unsplash-js';
 
 const useStyles = makeStyles(() => ({
+  weatherDashboardWrapper: {
+    width: "100%",
+    height: "100%",
+    backgroundImage: (image => `url(${image.url})`),
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center"
+  },
   topBar: {
     display: "flex",
     position: "relative"
@@ -15,50 +25,73 @@ const useStyles = makeStyles(() => ({
 }));
 
 function WeatherDashboard({props}) {
-  const classes = useStyles();
   const [forecastDuration, setForecastDuration] = useState("Today");
   const [dailyData, setDailyData] = useState([]);
   const [weeklyData, setWeeklyData] = useState([]);
   const [cityName, setCityName] = useState("Oslo");
+  const [image, setImage] = useState({
+    alt: 'A cloudy rainforest.',
+    url: Image,
+  })
 
-const handleChange = (event) => {
+  const classes = useStyles(image);
+
+  const handleChange = (event) => {
    setCityName(event.target.value);
-  }; 
+  };
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
 
   const handleDurationChange = duration => {
     setForecastDuration(duration);
   };
 
-useEffect(() => {
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
-   .then(response => response.json())
-      .then(response => {
-        if(!response || !response.list) return
-        const firstFive = response.list.slice(0, 5).map(listItem => {
-          return {
-            title: listItem.dt_txt,
-            iconID: listItem.weather[0].icon,
-            mainTemp: listItem.main.temp,
-            weatherDescription: listItem.weather[0].description
-          };
-        });
-        setDailyData(firstFive);
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+    .then(response => response.json())
+        .then(response => {
+          if(!response || !response.list) return
+          const firstFive = response.list.slice(0, 5).map(listItem => {
+            return {
+              title: listItem.dt_txt,
+              iconID: listItem.weather[0].icon,
+              mainTemp: listItem.main.temp,
+              weatherDescription: listItem.weather[0].description
+            };
+          });
+          setDailyData(firstFive);
 
-        const firstFiveDays = response.list.filter(listItem => listItem.dt_txt.includes("18:00:00")).map(listItem => {
-          return {
-            title: listItem.dt_txt,
-            iconID: listItem.weather[0].icon,
-            mainTemp: listItem.main.temp,
-            weatherDescription: listItem.weather[0].description
-          };
+          const firstFiveDays = response.list.filter(listItem => listItem.dt_txt.includes("18:00:00")).map(listItem => {
+            return {
+              title: listItem.dt_txt,
+              iconID: listItem.weather[0].icon,
+              mainTemp: listItem.main.temp,
+              weatherDescription: listItem.weather[0].description
+            };
+          });
+          setWeeklyData(firstFiveDays);
         });
-        setWeeklyData(firstFiveDays);
+
+      const unsplash = new Unsplash({
+        accessKey: `${process.env.REACT_APP_UNSPLASH_API_KEY}`
       });
-  }, [cityName]);
+
+      if (cityName) {
+        unsplash.search.photos(cityName,  1, 1,)
+          .then(toJson)
+          .then(json => {
+            const results = json.results
+            const imageUrl = results.length > 0 ? results[0].urls.full : Image
+
+            setImage({
+              alt: cityName,
+              url: imageUrl,
+            })
+          })
+      }
+    }, [cityName]);
 
   const day = new Date();
   const dayOptions = {day: "numeric" }
@@ -68,6 +101,7 @@ useEffect(() => {
 
   return (
     <>
+    <Box className={classes.weatherDashboardWrapper}>
       <Box className={classes.topBar}>
         <MonthDateBoxComponent day={day.toLocaleDateString(undefined,dayOptions)} month={month.toLocaleDateString(undefined,monthOptions)} />
          <SearchComponent value={cityName} handleChange={handleChange} handleSubmit={handleSubmit} /> 
@@ -88,8 +122,9 @@ useEffect(() => {
       <WeatherListComponent
         data={forecastDuration === "Today" ? dailyData : weeklyData}
       />
+  </Box>
     </>
   );
-}
+  }
 
 export default WeatherDashboard;

4f47493d05bf8d4fe9446f31949f5f7661e656ec
Justine Hill <justinehill@Justines-MacBook-Pro.local>
2020-09-29 10:33:36 +0200

    autocomplete  updates

--- src/components/Weather-Dashboard/Weather-Dashboard.js
+++ src/components/Weather-Dashboard/Weather-Dashboard.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import WeatherListComponent from "../Weather-List-Component/Weather-List-Component";
 import MonthDateBoxComponent from "../Month-Date-Box-Component/Month-Date-Box-Component";
-import SearchComponent from "../Search-Component/Search-Component";
 import CurrentCityConditionsDisplayComponent from "../Current-City-Conditions-Display-Component/Current-City-Conditions-Display-Component";
 import ForecastTabsComponent from "../Forecast-Tabs-Component/Forecast-Tabs-Component";
 import { Box } from "@material-ui/core";
 import Image from "../../../src/static/overcastCloudy.jpg";
 import Unsplash, { toJson }  from 'unsplash-js';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 const useStyles = makeStyles(() => ({
   weatherDashboardWrapper: {
@@ -21,28 +22,27 @@ const useStyles = makeStyles(() => ({
   topBar: {
     display: "flex",
     position: "relative"
+  }, 
+  autoCompleteWrapper: {
+    marginTop: "1rem",
+    marginLeft: "1rem"
   }
 }));
+
+const options = ["Oslo", "Perth", "Cape Town", "Graaff-Reinet", "London", "Portofino", "Sydney", "Bergen", "New York", "Paris", "Dublin"];
 
 function WeatherDashboard({props}) {
   const [forecastDuration, setForecastDuration] = useState("Today");
   const [dailyData, setDailyData] = useState([]);
   const [weeklyData, setWeeklyData] = useState([]);
-  const [cityName, setCityName] = useState("Oslo");
+  const [cityName, setCityName] = useState(options[0]);
+  const [inputValue, setInputValue] = useState("");
   const [image, setImage] = useState({
     alt: 'A cloudy rainforest.',
     url: Image,
   })
 
   const classes = useStyles(image);
-
-  const handleChange = (event) => {
-   setCityName(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  }
 
   const handleDurationChange = duration => {
     setForecastDuration(duration);
@@ -61,6 +61,7 @@ function WeatherDashboard({props}) {
               weatherDescription: listItem.weather[0].description
             };
           });
+          console.log(firstFive)
           setDailyData(firstFive);
 
           const firstFiveDays = response.list.filter(listItem => listItem.dt_txt.includes("18:00:00")).map(listItem => {
@@ -104,7 +105,22 @@ function WeatherDashboard({props}) {
     <Box className={classes.weatherDashboardWrapper}>
       <Box className={classes.topBar}>
         <MonthDateBoxComponent day={day.toLocaleDateString(undefined,dayOptions)} month={month.toLocaleDateString(undefined,monthOptions)} />
-         <SearchComponent value={cityName} handleChange={handleChange} handleSubmit={handleSubmit} /> 
+        <Box className={classes.autoCompleteWrapper}>
+      <Autocomplete
+        value={cityName}
+        onChange={(event, newValue) => {
+          setCityName(newValue);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        id="Search city"
+        options={options}
+        style={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Search city" variant="outlined" />}
+      />
+      </Box>
       </Box>
 
       {dailyData.slice(0,1).map(currentTempAndWeatherDescription => (

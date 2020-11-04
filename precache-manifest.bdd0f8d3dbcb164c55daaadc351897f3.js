37eb7bda09cf65f58c60f11eed60816866dff4d5
JustineAmber <justineamberhill@gmail.com>
2020-11-04 19:15:44 +0100

    Updates

---
+++ precache-manifest.bdd0f8d3dbcb164c55daaadc351897f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4bf063c442211ba315d7e114a55595",
+    "url": "/my-weather-app/index.html"
+  },
+  {
+    "revision": "51b1ec483b2db3e137f0",
+    "url": "/my-weather-app/static/css/main.8740b031.chunk.css"
+  },
+  {
+    "revision": "0e4995565c5fe621a60c",
+    "url": "/my-weather-app/static/js/2.310d2ba2.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/my-weather-app/static/js/2.310d2ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b1ec483b2db3e137f0",
+    "url": "/my-weather-app/static/js/main.36f73a50.chunk.js"
+  },
+  {
+    "revision": "405ecb9917fff97d2277",
+    "url": "/my-weather-app/static/js/runtime-main.8b6e1a51.js"
+  },
+  {
+    "revision": "66c5f23e51778ff2d6176cf108602e13",
+    "url": "/my-weather-app/static/media/overcastCloudy.66c5f23e.jpg"
+  }
+]);
